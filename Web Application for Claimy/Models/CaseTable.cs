79f75e86955b840e4bc7832b303982f9b3601f5b
MattyxD19 <mathiasbc97@gmail.com>
2019-11-28 11:21:04 +0100

    commit

---
+++ Web Application for Claimy/Models/CaseTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web_Api_Claimy.Models
+{
+    public class CaseTable
+    {
+    public string caseNo { get; set; }
+        [Required]
+    public string p_fineReason {get; set;}
+    public string precedens { get; set; }
+    public string status { get; set; }
+     // foreign key(s)  
+    public int empID { get; set; }
+    public int customerID { get; set; }
+     // navigation property '
+     public ClaimyEmployeeTable claimyEmployee { get; set; }
+     public CustomerTable customer { get; set; }
+
+}
+}
